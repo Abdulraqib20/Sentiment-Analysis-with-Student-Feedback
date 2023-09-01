197cd8c6ea2720d1cf1edc98ba1455d8329afcbb
raqibcodes <abdulraqibshakir03@gmail.com>
2023-08-31 18:22:46 -0700

    Update streamlit_sentiments.py

--- streamlit_sentiments.py
+++ streamlit_sentiments.py
@@ -9,6 +9,7 @@ import keras_nlp
 from transformers import BertTokenizer, TFBertForSequenceClassification
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
